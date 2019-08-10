a56b06f90db992281d7fb84721b330105b55c77a
Vladyslav Dumas <vladkyry@gmail.com>
2019-08-10 20:14:46 +0200

    - Making ContentPlayer non blocking and thread safe.

--- src/bot/ContentPlayerListener.kt
+++ src/bot/ContentPlayerListener.kt
@@ -4,7 +4,7 @@ interface ContentPlayerListener {
     fun onContentStart(content: Content)
     fun onContentStartFailure(content: Content)
     fun onContentStop(content: Content)
-    fun onContentEnd(content: Content)
+    suspend fun onContentEnd(content: Content)
     fun onContentPause(content: Content, position: Long)
     fun onContentResume(content: Content, position: Long)
     fun onContentPositionChange(content: Content, oldPosition: Long, newPosition: Long)
--- src/command/StopContentCommand.kt
+++ src/command/StopContentCommand.kt
@@ -1,8 +1,11 @@
 package fr.spoutnik87.command
 
+import fr.spoutnik87.bot.ContentPlayerState
+import fr.spoutnik87.bot.GetState
 import fr.spoutnik87.bot.Server
 import fr.spoutnik87.event.WebRequestEvent
 import fr.spoutnik87.reader.StopContentReader
+import kotlinx.coroutines.CompletableDeferred
 import org.slf4j.LoggerFactory
 
 class StopContentCommand : WebCommand {
@@ -12,7 +15,10 @@ class StopContentCommand : WebCommand {
     override suspend fun execute(event: WebRequestEvent, server: Server) {
         logger.debug("A command has been received on server ${server.guild.id.asString()}")
         val reader = event.payload as StopContentReader
-        if (reader.uid == server.player.getPlayingContent()?.uid) {
+        val response = CompletableDeferred<ContentPlayerState>()
+        server.player.send(GetState(response))
+        val state = response.await()
+        if (reader.uid == state.content?.uid) {
             server.stopPlayingContent()
             server.playNextContent()
         } else {
